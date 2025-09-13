84c3e0f52dc32f20508fd84092d6197a2beac151
JohnClark2001 <culla.johnclark.a@gmail.com>
2025-09-13 21:03:37 +0800

    Upload project files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'eaf4d64d27b41c0708142d849b8bc7cbf1e66c70',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'eaf4d64d27b41c0708142d849b8bc7cbf1e66c70',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
